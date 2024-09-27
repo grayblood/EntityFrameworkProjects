94a1c7e58d81f6326afe19be9f3d1654c4e7e79a
Grayblood <luismedinafernandes@gmail.com>
2024-09-27 18:54:53 +0200

    first commit

---
+++ EntityFramework/KingOfTokio/KingOfTokio/Context.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+
+namespace KingOfTokio
+{
+    class Context : DbContext
+    {
+        public Context() : base("tokio")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Partida> Partidas { get; set; }
+        public DbSet<Jugador> Jugadores { get; set; }
+        public DbSet<Monstre> Monstres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+
+        }
+
+
+    }
+
+}
+
